6dc8307c2636a8d4dc51607e4b9092b6929d4cbc
zjx492357816 <2564342922@qq.com>
2021-05-09 19:10:02 +0800

    第六次：放入图片和音乐

---
+++ game4/dialogh.cpp
@@ -0,0 +1,21 @@
+#include "dialogh.h"
+#include "ui_dialogh.h"
+#include<QPixmap>
+Dialogh::Dialogh(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialogh)
+{
+    ui->setupUi(this);
+    ui->label->setPixmap(QPixmap(":/images/p4.jpg"));
+    ui->label->setScaledContents(true);
+}
+
+Dialogh::~Dialogh()
+{
+    delete ui;
+}
+
+void Dialogh::on_pushButton_clicked()
+{
+    this->close();
+}
